08ec00c808f95a357db72d18c2e83ced8b5a90fa
Tom Robinson <tarobins@gmail.com>
2025-06-14 22:17:40 -0700

    Try debug

--- 2/2.cc
+++ 2/2.cc
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "flags/flags.h"
 #include "table_read/table_read.h"
 
 int process(Table table) {
+    int numberOfSafeRows = 0;
     // Output the table contents
     for (const Row &row : table) {
-        for (const int &cell : row) {
-            std::cout << cell << " ";
+        ConstColItr colItr = row.begin();
+        const int lastValue = *colItr;
+        colItr++;
+        while (true) {
+            const int diff = std::abs(lastValue - *colItr);
+            if (!(diff == 1 || diff == 2)) {
+                break; // Skip rows that are not safe
+            } else {
+                colItr++;
+                if (colItr == row.end()) {
+                    numberOfSafeRows++;
+                    break; // Safe row found, exit the loop
+                }
+            }
         }
-        std::cout << std::endl;
+        
     }
+    std::cout << "Number of safe rows: " << numberOfSafeRows << std::endl;
     return 0;
 }
