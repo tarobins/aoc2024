9b2b18255c9be8e035dc71f845e8262ffd53584d
Tom Robinson <tarobins@gmail.com>
2025-06-15 22:04:34 +0000

    part 2 a

--- 2/2.cc
+++ 2/2.cc
@@ -4,27 +4,48 @@
 #include "flags/flags.h"
 #include "table_read/table_read.h"
 
+enum class Direction {
+    UP,
+    DOWN, 
+    UNKNOWN
+};
+
 int process(Table table) {
     int numberOfSafeRows = 0;
     // Output the table contents
     for (const Row &row : table) {
+        Direction direction = Direction::UNKNOWN;
         ConstColItr colItr = row.begin();
-        const int lastValue = *colItr;
+        int lastValue = *colItr;
         colItr++;
         while (true) {
-            const int diff = std::abs(lastValue - *colItr);
-            if (!(diff == 1 || diff == 2)) {
-                break; // Skip rows that are not safe
-            } else {
-                colItr++;
-                if (colItr == row.end()) {
-                    numberOfSafeRows++;
-                    break; // Safe row found, exit the loop
+            const int diff = *colItr - lastValue;
+            if (direction == Direction::UNKNOWN) {
+                if (diff == 1 || diff == 2 || diff == 3) {
+                    direction = Direction::UP; // Initial direction
+                } else if (diff == -1 || diff == -2 || diff == -3) {
+                    direction = Direction::DOWN; // Initial direction
+                } else {
+                    break; // Skip rows that are not safe
+                }
+            } else if (direction == Direction::UP) {
+                if (diff <= 0 || diff > 3) {
+                    break; // Skip rows that are not safe
+                }
+            } else if (direction == Direction::DOWN) {
+                if (diff >= 0 || diff < -3) {
+                    break; // Skip rows that are not safe
                 }
             }
+            lastValue = *colItr;
+            if (++colItr == row.end()) {
+                // If we reach the end of the row, it is a safe row
+                numberOfSafeRows++;
+                break;
+            }
         }
-        
     }
+
     std::cout << "Number of safe rows: " << numberOfSafeRows << std::endl;
     return 0;
 }
