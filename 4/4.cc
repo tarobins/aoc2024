6d740cb5431d7f9fa74d88d3f1e9070075c22be6
Tom Robinson <tarbins@gmail.com>
2025-06-19 21:42:42 -0700

    Day 4 framework

---
+++ 4/4.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cerr << "This is the 4/4.cc file." << std::endl;
+    std::cerr << "It is a placeholder for the final part of the project." << std::endl;
+    
+    // Placeholder for further implementation
+    // This could include reading a file, processing data, etc.
+    
+    return 0;
+}
