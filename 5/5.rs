296dbd877f6235d9bcc49cd9feae98d50bfef570
Tom Robinson <tarobins@gmail.com>
2025-06-29 20:40:13 +0000

    feat(5): Sum center element of valid vectors
    Modify the logic to sum only the center-most element of each valid
    vector and provide a clear breakdown of the calculation.

--- 5/5.rs
+++ 5/5.rs
@@ -36,4 +36,47 @@ fn main() {
 
     println!("Tuples: {:?}", tuples);
     println!("Vectors: {:?}", vectors);
+
+    let mut valid_vectors: Vec<Vec<i32>> = Vec::new();
+    for vector in &vectors {
+        let mut is_vector_valid = true;
+        if vector.len() < 2 {
+            is_vector_valid = false;
+        } else {
+            'pair_loop: for i in 0..vector.len() - 1 {
+                let current_pair = (vector[i], vector[i + 1]);
+                let mut pair_found_in_tuples = false;
+                for &tuple in &tuples {
+                    if tuple == current_pair {
+                        pair_found_in_tuples = true;
+                        break;
+                    }
+                }
+
+                if !pair_found_in_tuples {
+                    is_vector_valid = false;
+                    break 'pair_loop;
+                }
+            }
+        }
+
+        if is_vector_valid {
+            valid_vectors.push(vector.clone());
+        }
+    }
+
+    println!("Valid Vectors: {:?}", valid_vectors);
+
+    let mut total_sum = 0;
+    println!("\nCalculating sum of center elements:");
+    for vector in &valid_vectors {
+        if !vector.is_empty() {
+            let center_index = vector.len() / 2;
+            let center_element = vector[center_index];
+            println!("  - For vector {:?}, center element is {}, adding to sum.", vector, center_element);
+            total_sum += center_element;
+        }
+    }
+
+    println!("\nTotal sum of center elements: {}", total_sum);
 }
